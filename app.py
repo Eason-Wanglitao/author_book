666734faeac65a8cac197bb70ffe058b1f1ae372
王立涛 <1195855531@qq.com>
2019-09-18 18:42:47 +0800

    修改数量

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
+import config
 
 app = Flask(__name__)
-
+app.config.from_object(config)
 
 @app.route('/')
 def hello_world():
+    return redirect('/login/')
     return 'Hello World!'
+
+
+@app.route('/login/')
+def login():
+    return '这是登录页面'
+
+@app.route('/article/<id>')
+def article(id):
+    return '参数是%s' % id
 
 
 if __name__ == '__main__':
